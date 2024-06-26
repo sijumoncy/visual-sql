1f6c4d821de07e5e9f786efc62a9e0d0d4672dcb
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-06-26 16:41:34 +0530

    component configuration and taiwlind

--- src/components/Sidebar/SidebarTree/SidebarTree.tsx
+++ src/components/Sidebar/SidebarTree/SidebarTree.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { data } from "../../../data/treeData";
+import React, { useState } from "react";
+// import { data } from "../../../data/treeData";
 import { SquarePlus } from "lucide-react";
 import {
   DragTypeEnum,
   ITable,
-  ITableGroup,
+  ICollection,
 } from "../../../interface/tableData";
 
 function SidebarTree() {
-  const tableData = data as ITableGroup[];
+  const [collection, setCollection] = useState<ICollection | null>(null);
+
+  const [showCollectionForm, setCollectionForm] = useState(false);
 
   const handleDragTable = (
     e: React.DragEvent<HTMLLIElement>,
@@ -25,7 +27,23 @@ function SidebarTree() {
 
   return (
     <div className="w-full">
-      {tableData && tableData.length > 0 ? (
+      {/* collections */}
+      <div className="flex justify-between px-1">
+        <p>Collections</p>
+        <button title="Create a table collection">
+          <SquarePlus
+            className="text-gray-700 hover:text-gray-900 cursor-pointer"
+            onClick={() => setCollectionForm(true)}
+          />
+        </button>
+      </div>
+
+      {/* create collection form */}
+      <div className="my-2">
+        
+      </div>
+
+      {/* {tableData && tableData.length > 0 ? (
         <ul className="flex flex-col gap-5">
           {tableData.map(({ id, name, tables }: ITableGroup) => (
             <div key={id}>
@@ -53,7 +71,7 @@ function SidebarTree() {
         </ul>
       ) : (
         <div>No Table Exist.Create One to start</div>
-      )}
+      )} */}
     </div>
   );
 }
--- src/interface/tableData.ts
+++ src/interface/tableData.ts
@@ -6,23 +6,29 @@ export interface IColumn {
   column_data_type: ColumDataTypes;
 }
 
+export interface ICollection {
+  collectionId: string;
+  collectionName: string;
+  tables: ITable[];
+}
+
 export interface ITable {
   id: string;
   name: string;
   columns: IColumn[];
 }
 
-export interface ITableGroup {
-  id: string;
-  name: string;
-  tables: ITable[];
-}
+// export interface ITableGroup {
+//   id: string;
+//   name: string;
+//   tables: ITable[];
+// }
 
 export enum DragTypeEnum {
   TABLE_NODE = "drag type id sidebar table node",
   TABLE_COLUMN = "drag type id column drag",
   DRAGGED_TABLE_DATA = "key for drag event data transfer",
-  DRAGGED_TABLE_COLUMN="key for drag column event data transfer"
+  DRAGGED_TABLE_COLUMN = "key for drag column event data transfer",
 }
 
 export enum CustomNodeTypes {
